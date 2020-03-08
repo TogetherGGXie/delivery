b7dfc6016a8921bf158debd8dccf247b15d47b1e
ermao <1563024120@qq.com>
2020-03-08 10:33:04 +0800

    接口修改

--- src/main/java/com/delivery/main/common/persistence/dao/RestaurantMapper.java
+++ src/main/java/com/delivery/main/common/persistence/dao/RestaurantMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.plugins.pagination.Pagination;
 import com.delivery.main.common.persistence.template.modal.Restaurant;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.HashMap;
@@ -23,7 +24,7 @@ public interface RestaurantMapper extends BaseMapper<Restaurant> {
     @Select("select * from restaurant order by create_time")
     List<HashMap<String, Object>> getRestaurantList(Pagination pagination);
 
-    @Select("SELECT * from restaurant " +
-            "where (lat between lat-0.0048 and lat+0.0048) and (lng between lng-0.0048 and lng+0.0048)")
-    List<Restaurant> queryList(String lng,String lat);
+//    @Select("SELECT * from restaurant " +
+//            "where (lat between lat-0.0048 and lat+0.0048) and (lng between lng-0.0048 and lng+0.0048)")
+    List<Restaurant> queryList(@Param("lng") String lng,@Param("lat") String lat);
 }
